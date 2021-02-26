027df271aa6fa4525cdce5c6a642a5e4fd26477c
alextodd434 <alextodd434@gmail.com>
2021-02-26 16:29:34 +0000

    Sets coordinates for square

--- opencv_reverse.py
+++ opencv_reverse.py
@@ -35,11 +35,6 @@ def mousepoints(event, x, y, flags, params):
         ledClicked = True
         LED_x = x
         LED_y = y
-
-        x1 = int(LED_x - r_size / 2)
-        x2 = int(LED_x + r_size / 2)
-        y1 = int(LED_y - r_size / 2)
-        y2 = int(LED_y + r_size / 2)
         print("Button clicked")
 
 # Window with live camera feed
@@ -47,8 +42,9 @@ while True:
     ret, frame = cap.read()
     cv2.setMouseCallback("Camera Feed", mousepoints)
     if ledClicked:
-        cv2.rectangle(frame, (x1, y1), (x2, y2), r_colour, r_thickness)
-        crop = frame[y1:y2, x1:x2]
+        sqc = coordinatefunction(LED_x, LED_y, r_size)
+        cv2.rectangle(frame, (sqc[0], sqc[2]), (sqc[1], sqc[3]), r_colour, r_thickness)
+        crop = frame[sqc[2]:sqc[3], sqc[0]:sqc[1]]
         cv2.imshow("test crop", crop)
 
     cv2.imshow("Camera Feed", frame)

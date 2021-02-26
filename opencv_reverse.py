d272680409f138d39b0d570d98b337029c96f886
JakeBroughton <46027440+JakeBroughton@users.noreply.github.com>
2021-02-26 16:23:49 +0000

    added cropping to opencv_reverse.py

--- opencv_reverse.py
+++ opencv_reverse.py
@@ -10,12 +10,13 @@ cameraID = 0
 ledClicked = False
 
 # Rectangle size
-r_size = 25
+r_size = 50
 r_colour = (0, 128, 0)
 r_thickness = 2
 
 # LED Location
 LED_x, LED_y = 0, 0
+x1, x2, y1, y2 = 0, 0, 0, 0
 
 # Window height and width
 h, w = 480, 480
@@ -29,11 +30,16 @@ cameracheck(cameraID)
 
 # Mouse clicking
 def mousepoints(event, x, y, flags, params):
-    global LED_x, LED_y, ledClicked
+    global LED_x, LED_y, ledClicked, x1, x2, y1, y2
     if event == cv2.EVENT_LBUTTONDBLCLK:
         ledClicked = True
         LED_x = x
         LED_y = y
+
+        x1 = int(LED_x - r_size / 2)
+        x2 = int(LED_x + r_size / 2)
+        y1 = int(LED_y - r_size / 2)
+        y2 = int(LED_y + r_size / 2)
         print("Button clicked")
 
 
@@ -42,9 +48,9 @@ while True:
     ret, frame = cap.read()
     cv2.setMouseCallback("Camera Feed", mousepoints)
     if ledClicked:
-        cv2.rectangle(frame, (LED_x - r_size, LED_y - r_size), (LED_x + r_size, LED_y + r_size), r_colour, r_thickness)
-
-        crop_image = frame[]
+        cv2.rectangle(frame, (x1, y1), (x2, y2), r_colour, r_thickness)
+        crop = frame[y1:y2, x1:x2]
+        cv2.imshow("test crop", crop)
 
     cv2.imshow("Camera Feed", frame)
 
